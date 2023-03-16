5a6c010e756ca3e643d070c0792954f60697511b
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-03-16 16:51:33 +0530

    updating

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from "express"
 import colors from "colors"
+import dotenv from "dotenv"
+//configure env
+dotenv.config();
 const app=express()
 //rest api
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to amazing</h1>")
 })
 //Port
-const PORT=8080
+const PORT=process.env.PORT||8080;
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`.bgCyan.white)
 })

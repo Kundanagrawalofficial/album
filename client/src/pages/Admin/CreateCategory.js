66e98bc04aa41d0a9f94cf806e4ebafeee88131e
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-03-18 22:17:40 +0530

    updating

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -4,24 +4,23 @@ import AdminMenu from "./../../components/Layout/AdminMenu";
 import  toast from 'react-hot-toast';
 import axios from "axios"
 const CreateCategory = () => {
-  const [categories,setCategories]= useState([]);
+  const [categories, setCategories] = useState([]);
   //get all cat
-  const getAllcategory =async () =>{
+  const getAllCategory = async () => {
     try {
-      const { data}= await axios.get('/api/v1/category/get-category');
-      if(data.success){
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
         setCategories(data.category);
       }
     } catch (error) {
       console.log(error);
-      toast.error('Something went Wrong in getting category')
-      
+      toast.error("Something wwent wrong in getting catgeory");
     }
   };
-  useEffect(()=>{
-   getAllcategory();
 
-  },[]);
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">

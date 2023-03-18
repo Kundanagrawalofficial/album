396f87072f1568c9911bc0f233204c79ce325d1f
Kundanagrawalofficial <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-03-18 18:53:38 +0530

    adding updated category and user crud opertaion in mongodb

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import morgan from "morgan"
 import conncetDB from './config/db.js';
 import authRoutes from "./routes/authRoute.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
 import cors from "cors"
 //configure env
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 //routes
 app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/category',categoryRoutes)
 //rest api
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to amazing</h1>")

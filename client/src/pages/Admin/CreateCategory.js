02cd533633b592a29351ba6a4341bb27467b909f
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-03-18 23:05:26 +0530

    ok

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,26 +1,32 @@
-import React ,{useEffect,useState}from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 import AdminMenu from "./../../components/Layout/AdminMenu";
-import  toast from 'react-hot-toast';
-import axios from "axios"
+import toast from "react-hot-toast";
+import axios from "axios";
+
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
-  //get all cat
-  const getAllCategory = async () => {
+
+  //get all categories
+  const getAllCategories = async () => {
     try {
-      const { data } = await axios.get('/api/v1/category/get-category');
+      const { data } = await axios.get("/api/v1/category/get-category");
       if (data.success) {
         setCategories(data.category);
       }
     } catch (error) {
       console.log(error);
-      toast.error("Something wwent wrong in getting catgeory");
+      console.log(error.response);
+      toast.error("Something went wrong in getting category");
     }
   };
 
   useEffect(() => {
-    getAllCategory();
+    getAllCategories();
   }, []);
+
+  console.log(categories);
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -31,27 +37,26 @@ const CreateCategory = () => {
           <div className="col-md-9">
             <h1>Manage Category</h1>
             <div className="w-75">
-            <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-      {categories?.map((c) =>(
-        <>
-          <tr>
-         <td key={c.id}>{c.name}</td>
-          <td>
-         <button className="btn btn-primary">Edit</button>
-         </td>
-         </tr>
-         </>
-      ))}
-  </tbody>
-</table>
-
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr>
+                        <td key={c.id}>{c.name}</td>
+                        <td>
+                          <button className="btn btn-primary">Edit</button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>

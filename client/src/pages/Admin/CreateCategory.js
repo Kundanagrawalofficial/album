7197ef2e9032fcbbd175e9c68e81d31470c6865b
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-03-18 22:03:58 +0530

    yaa

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -10,7 +10,7 @@ const CreateCategory = () => {
     try {
       const {data}= await axios.get('/api/v1/category/get-category')
       if(data.success){
-        setCategories(data);
+        setCategories(data.category);
       }
     } catch (error) {
       console.log(error)
